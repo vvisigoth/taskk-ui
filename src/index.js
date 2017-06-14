d2a37a4b4d1182785ff4ca9abfdcd0802447b650
Anthony <anthony@ai.co>
2017-06-14 07:55:24 -0700

    asyn populate from json

--- src/index.js
+++ src/index.js
@@ -7,9 +7,25 @@ import './index.css';
 import {Provider} from 'react-redux';
 import Store from './store';
 
-console.debug(Store);
-
 const StoreInstance = Store();
+
+const toJson = (r) => {
+  let j = r.json();
+  return j
+};
+
+const populate = (j) => {
+  Object.keys(j.board).forEach(k => {
+    StoreInstance.dispatch({type: 'CREATE_PHASE', phase: k});
+    j.board[k].forEach(j => {
+      StoreInstance.dispatch({type: 'CREATE_ISSUE', phase: k, issueObj: j})
+    });
+  });
+  console.debug(StoreInstance.getState());
+};
+
+const json = fetch('./dummyBoard.json').then(toJson).then(populate);
+
 
 ReactDOM.render(
   <Provider store={StoreInstance}> 
