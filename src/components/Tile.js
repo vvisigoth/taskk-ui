3eb547fab97c87295221806f3ddad3a1b5662b4b
Anthony <anthony@ai.co>
2017-06-29 11:27:00 -0700

    update issueObj on titel change

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -192,8 +192,8 @@ class Tile extends Component {
     return (
       <div ref='tile' className={this.classNames()} onDoubleClick={this.handleClick} draggable="true" onDragStart={this.handleDragStart} onDragOver={this.handleDragOver} onDrop={this.handleDrop} style={{ marginTop: this.state.bumpAmt}} > 
         <div className="tile-container"> 
-          <input className="title" type="text" ref="title" value={this.props.issue ? this.props.issue.title : ''}/> 
-          <input className="author" type="text" ref="author" value={this.props.issue ? this.props.issue.author: ''}/> 
+          <input className="title" type="text" ref="title" onChange={this.handleChange} defaultValue={this.props.issue ? this.props.issue.title : ''}/> 
+          <input className="author" type="text" ref="author" onChange={this.handleChange} defaultValue={this.props.issue ? this.props.issue.author: ''}/> 
         </div> 
         <div className="indicator"></div> 
         <textarea className="description" ref="description" onChange={this.handleChange} defaultValue={this.props.issue ? this.props.issue.description : ''}></textarea> 
